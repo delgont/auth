6faf8e4208306e020639814b04fcb0d880fad1d1
Stephen Okello Omoding <okellostephenomoding@gmail.com>
2024-07-11 15:30:58 +0300

    Username column configuration

--- src/Concerns/MultiAuthCredentials.php
+++ src/Concerns/MultiAuthCredentials.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 trait MultiAuthCredentials
 {
+
      /**
      * Get the needed authorization credentials from the request.
      *
@@ -14,12 +15,21 @@ trait MultiAuthCredentials
      */
     protected function multiAuthCredentials(Request $request)
     {
-        $username = filter_var($request->{method_exists($this, 'username') ? $this->username() : 'email'}, FILTER_VALIDATE_EMAIL) ? 'email' : config('auth.username', 'name');
+        $username = filter_var($request->{method_exists($this, 'username') ? $this->username() : 'email'}, FILTER_VALIDATE_EMAIL) ? 'email' : config($this->getConfig().'.username', 'name');
 
         return [
          $username => $request->{method_exists($this, 'username') ? $this->username() : 'email'},
          'password' => $request->password
         ];
     }
-    
+
+    /**
+     * Get the configuration file where the username is defined
+     * @return string
+     */
+    protected function getConfig()
+    {
+        return 'auth';
+    }
+
 }
